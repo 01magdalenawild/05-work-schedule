7b145e55ba05c298efbe294d370d67fb8c75a144
Magdalena Wild <maggieprophete2425@gmail.com>
2022-10-11 19:04:40 -0400

    added changes to javascript for rows containing the time, save button and description

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var weekDay = moment().format("ddd MMM Do, YYYY");
+var currenthour=moment().hour ()
+console.log(currenthour)
+$("#currentDay").text(weekDay);
+function createrows(){
+    for(var i=7;i<15;i++){
+        var currently;
+        if(i<currenthour){
+            currently="past"
+    
+        } else if(i===currenthour){
+            currently="present"
+        }
+        else{
+            currently="future"
+        }
+        var time=i+" am"
+        if (i>12){
+            time=i-12+" pm"
+        }
+   $(".container").append("<div class='row "+currently+"'><h5 class='col-1'>"+time+"</h5><textarea class='col-10'></textarea><button class='col-1'>💾</button></div>")
+    }
+}
+createrows()
